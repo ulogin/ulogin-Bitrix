10e7239f3ab5f466a19e8030454bde68bb49043a
uLogin Team <team@ulogin.ru>
2012-08-09 13:08:01 +0400

    update commit

--- ulogin/auth/component.php
+++ ulogin/auth/component.php
@@ -12,155 +12,160 @@ if (!empty($_POST['token']) && !$USER->isAuthorized()) {
     $s = file_get_contents('http://ulogin.ru/token.php?token=' . $_POST['token'] . '&host=' . $_SERVER['HTTP_HOST']);
     $profile = json_decode($s, true);
 
-    list($d, $m, $y) = explode('.', $profile['bdate']);
+    if (count($profile) && !isset($profile['error'])){
 
-    $arResult['USER']['LOGIN'] = Ulogin::genNickname($profile);
-    $arResult['USER']['NAME'] = $profile['first_name'];
-    $arResult['USER']['LAST_NAME'] = $profile['last_name'];
-    $arResult['USER']['EMAIL'] = $profile['email'];
-    $arResult['USER']['PERSONAL_GENDER'] = ($profile['sex'] == 2 ? 'M' : 'F');
-    $arResult['USER']['PERSONAL_CITY'] = $profile['city'];
-    $arResult['USER']['PERSONAL_BIRTHDAY'] = $d . '.' . $m . '.' . $y;
-    $arResult['USER']['EXTERNAL_AUTH_ID'] = $profile['identity'];
-    $arResult['USER']['PHOTO'] = $profile['photo'];
-    $arResult['USER']['PHOTO_BIG'] = $profile['photo_big'];
-    $arResult['USER']['NETWORK'] = $profile['network'];
-    // проверяем есть ли пользователь в БД.	Если есть - то авторизуем, нет  - регистрируем и авторизуем
-    $rsUsers = CUser::GetList(
-        ($by = "email"),
-        ($order = "desc"),
-        array(
-            "EXTERNAL_AUTH_ID" => $arResult['USER']["EXTERNAL_AUTH_ID"],
-        )
-    );
-    $arUser = $rsUsers->GetNext();
-    
-    $emailExist = false;
-    // проверка уникальности email 
-    if ($arParams['UNIQUE_EMAIL'] == 'Y'){
-      $emailUsers = CUser::GetList(
-	                        ($by = "id"),
-	                        ($order = "desc"),
-	                        array(
-                              "EMAIL" => $arResult['USER']["EMAIL"],
-                              "ACTIVE" => "Y"
-	                        )
-                    );
-      if (intval($emailUsers->SelectedRowsCount()) > 0){
-        $emailExist = true;
-      }
-    }
+        list($d, $m, $y) = explode('.', $profile['bdate']);
 
-    if ($arUser["EXTERNAL_AUTH_ID"] == $arResult['USER']["EXTERNAL_AUTH_ID"]) {
-
-        // такой пользователь есть, авторизуем пользователя
-
-        $ID_INFO = explode('=',$arUser['ADMIN_NOTES']);
-
-        if ($arResult['USER']['NETWORK'] == $ID_INFO[0] && $arUser['ACTIVE'] == 'Y'){//старый формат хранения аккаунтов, конвертируем
-
-            $USER->Update($arUser['ID'], array('EXTERNAL_AUTH_ID'=>''));
-            Ulogin::createUloginAccount($arResult['USER'], $arUser['ID']);
-            $ID_INFO[1] = $arUser['ID'];
-        }
-
-        $USER->Authorize($ID_INFO[1]);
-
-        if ($arParams["REDIRECT_PAGE"] != "")
-            LocalRedirect($arParams["REDIRECT_PAGE"]);
-        else
-            LocalRedirect($APPLICATION->GetCurPageParam("", array("logout")));
-
-    }else if (!$emailExist){
-        // регистрируем пользователя, и добавляем его в группы, указанные в параметрах
-        $user = new CUser;
-        $GroupID = "5";
-        $passw = rand(1000000,10000000);
-
-        if (is_array($arParams["GROUP_ID"]))
-            $GroupID = $arParams["GROUP_ID"];
-	if (!is_array($GroupID)) $GroupID=array($GroupID);
-
-        if (!$arResult['USER']["EMAIL"])
-            $arResult['USER']["EMAIL"] = "yourmail@domain.com";
-
-        # проверяем есть ли такой логин
+        $arResult['USER']['LOGIN'] = Ulogin::genNickname($profile);
+        $arResult['USER']['NAME'] = $profile['first_name'];
+        $arResult['USER']['LAST_NAME'] = $profile['last_name'];
+        $arResult['USER']['EMAIL'] = $profile['email'];
+        $arResult['USER']['PERSONAL_GENDER'] = ($profile['sex'] == 2 ? 'M' : 'F');
+        $arResult['USER']['PERSONAL_CITY'] = $profile['city'];
+        $arResult['USER']['PERSONAL_BIRTHDAY'] = $d . '.' . $m . '.' . $y;
+        $arResult['USER']['EXTERNAL_AUTH_ID'] = $profile['identity'];
+        $arResult['USER']['PHOTO'] = $profile['photo'];
+        $arResult['USER']['PHOTO_BIG'] = $profile['photo_big'];
+        $arResult['USER']['NETWORK'] = $profile['network'];
+        // проверяем есть ли пользователь в БД.	Если есть - то авторизуем, нет  - регистрируем и авторизуем
         $rsUsers = CUser::GetList(
             ($by = "email"),
             ($order = "desc"),
             array(
-                "LOGIN" => $arResult['USER']["LOGIN"],
-                "ACTIVE" => "Y"
+                "EXTERNAL_AUTH_ID" => $arResult['USER']["EXTERNAL_AUTH_ID"]
             )
         );
+        $arUser = $rsUsers->GetNext();
 
-        while ($arUser = $rsUsers->GetNext())
-            $count_user_id[] = $arUser["ID"];
-
-        if (count($count_user_id) > 0) {
-            $arResult['USER']["LOGIN"] = $arResult['USER']["LOGIN"] . "_" . count($count_user_id);
+        $emailExist = false;
+        // проверка уникальности email
+        if ($arParams['UNIQUE_EMAIL'] == 'Y'){
+          $emailUsers = CUser::GetList(
+                                ($by = "id"),
+                                ($order = "desc"),
+                                array(
+                                  "EMAIL" => $arResult['USER']["EMAIL"],
+                                  "ACTIVE" => "Y"
+                                )
+                        );
+          if (intval($emailUsers->SelectedRowsCount()) > 0){
+            $emailExist = true;
+          }
         }
 
-        $imageContent = file_get_contents($profile['photo']);
-            $ext = strtolower(substr($profile['photo'],-3));
-            if (!in_array($ext,array('jpg','jpeg','png','gif','bmp'))) $ext = 'jpg';
+        if ($arUser["EXTERNAL_AUTH_ID"] == $arResult['USER']["EXTERNAL_AUTH_ID"]) {
 
-            $tmpName = $tmpName = rand(100000,10000000).'.'.$ext;
-            $tmpName = $_SERVER["DOCUMENT_ROOT"]."/images/".$tmpName;
+            // такой пользователь есть, авторизуем пользователя
 
-        file_put_contents($tmpName,$imageContent);
+            $ID_INFO = explode('=',$arUser['ADMIN_NOTES']);
 
-        $arIMAGE = CFile::MakeFileArray($tmpName);
-        $arIMAGE["MODULE_ID"] = "main";
+            if ($arResult['USER']['NETWORK'] == $ID_INFO[0] && $arUser['ACTIVE'] == 'Y'){//старый формат хранения аккаунтов, конвертируем
 
-	
-        $arFields = Array(
-            "NAME" => $arResult['USER']['NAME'],
-            "LAST_NAME" => $arResult['USER']['LAST_NAME'],
-            "EMAIL" => $arResult['USER']["EMAIL"],
-            "LOGIN" => $arResult['USER']["LOGIN"],
-            "PERSONAL_GENDER" => $arResult['USER']["PERSONAL_GENDER"],
-            "ADMIN_NOTES" => $arResult['USER']["NETWORK"],
-            "PERSONAL_BIRTHDAY" => $arResult['USER']['PERSONAL_BIRTHDAY'],
-            "ACTIVE" => "Y",
-            "GROUP_ID" => $GroupID,
-            "PASSWORD" => $passw,
-            "CONFIRM_PASSWORD" => $passw,
-            "PERSONAL_PHOTO"    => $arIMAGE,
-        );
+                $USER->Update($arUser['ID'], array('EXTERNAL_AUTH_ID'=>''));
+                Ulogin::createUloginAccount($arResult['USER'], $arUser['ID']);
+                $ID_INFO[1] = $arUser['ID'];
+            }
 
-        $UserID = $user->Add($arFields);
-	if ($UserID && $arParams['SEND_EMAIL'] == 'Y'){
-	  $arEventFields = array(
-	    'USER_ID' => $UserID,
-	    'LOGIN' => $arFields['LOGIN'],
-	    'EMAIL' => $arFields['EMAIL'],
-	    'NAME' => $arFields['NAME'],
-	    'LAST_NAME' => $arFields['LAST_NAME'],
-	    'USER_IP'  => '',
-	    'USER_HOST'  => ''
-	  );
-	  $event = new CEvent;
-	  $msg = $event->SendImmediate("NEW_USER", SITE_ID, $arEventFields);
-	  ShowMessage($msg);
-
-	}
-        unlink($tmpName);
-
-        if (intval($UserID) > 0) {
-            $arFields['EXTERNAL_AUTH_ID'] = $arResult['USER']["EXTERNAL_AUTH_ID"];
-            Ulogin::createUloginAccount($arResult['USER'], $UserID);
-            $USER->Authorize($UserID);
+            $USER->Authorize($ID_INFO[1]);
 
             if ($arParams["REDIRECT_PAGE"] != "")
                 LocalRedirect($arParams["REDIRECT_PAGE"]);
             else
                 LocalRedirect($APPLICATION->GetCurPageParam("", array("logout")));
 
+        }else if (!$emailExist){
+            // регистрируем пользователя, и добавляем его в группы, указанные в параметрах
+            $user = new CUser;
+            $GroupID = "5";
+            $passw = rand(1000000,10000000);
+
+            if (is_array($arParams["GROUP_ID"]))
+                $GroupID = $arParams["GROUP_ID"];
+        if (!is_array($GroupID)) $GroupID=array($GroupID);
+
+            if (!$arResult['USER']["EMAIL"])
+                $arResult['USER']["EMAIL"] = "yourmail@domain.com";
+
+            # проверяем есть ли такой логин
+            $rsUsers = CUser::GetList(
+                ($by = "email"),
+                ($order = "desc"),
+                array(
+                    "LOGIN" => $arResult['USER']["LOGIN"],
+                    "ACTIVE" => "Y"
+                )
+            );
+
+            while ($arUser = $rsUsers->GetNext())
+                $count_user_id[] = $arUser["ID"];
+
+            if (count($count_user_id) > 0) {
+                $arResult['USER']["LOGIN"] = $arResult['USER']["LOGIN"] . "_" . count($count_user_id);
+            }
+
+            $imageContent = file_get_contents($profile['photo']);
+                $ext = strtolower(substr($profile['photo'],-3));
+                if (!in_array($ext,array('jpg','jpeg','png','gif','bmp'))) $ext = 'jpg';
+
+                $tmpName = $tmpName = rand(100000,10000000).'.'.$ext;
+                $tmpName = $_SERVER["DOCUMENT_ROOT"]."/images/".$tmpName;
+
+            file_put_contents($tmpName,$imageContent);
+
+            $arIMAGE = CFile::MakeFileArray($tmpName);
+            $arIMAGE["MODULE_ID"] = "main";
+
+
+            $arFields = Array(
+                "NAME" => $arResult['USER']['NAME'],
+                "LAST_NAME" => $arResult['USER']['LAST_NAME'],
+                "EMAIL" => $arResult['USER']["EMAIL"],
+                "LOGIN" => $arResult['USER']["LOGIN"],
+                "PERSONAL_GENDER" => $arResult['USER']["PERSONAL_GENDER"],
+                "ADMIN_NOTES" => $arResult['USER']["NETWORK"],
+                "PERSONAL_BIRTHDAY" => $arResult['USER']['PERSONAL_BIRTHDAY'],
+                "ACTIVE" => "Y",
+                "GROUP_ID" => $GroupID,
+                "PASSWORD" => $passw,
+                "CONFIRM_PASSWORD" => $passw,
+                "PERSONAL_PHOTO"    => $arIMAGE,
+            );
+
+            $UserID = $user->Add($arFields);
+        if ($UserID && $arParams['SEND_EMAIL'] == 'Y'){
+          $arEventFields = array(
+            'USER_ID' => $UserID,
+            'LOGIN' => $arFields['LOGIN'],
+            'EMAIL' => $arFields['EMAIL'],
+            'NAME' => $arFields['NAME'],
+            'LAST_NAME' => $arFields['LAST_NAME'],
+            'USER_IP'  => '',
+            'USER_HOST'  => ''
+          );
+          $event = new CEvent;
+          $msg = $event->SendImmediate("NEW_USER", SITE_ID, $arEventFields);
+          ShowMessage($msg);
+
         }
+            unlink($tmpName);
 
+            if (intval($UserID) > 0) {
+                $arFields['EXTERNAL_AUTH_ID'] = $arResult['USER']["EXTERNAL_AUTH_ID"];
+                Ulogin::createUloginAccount($arResult['USER'], $UserID);
+                $USER->Authorize($UserID);
+
+                if ($arParams["REDIRECT_PAGE"] != "")
+                    LocalRedirect($arParams["REDIRECT_PAGE"]);
+                else
+                    LocalRedirect($APPLICATION->GetCurPageParam("", array("logout")));
+
+            }
+
+        }
+    }else{
+        if (isset($profile['error']))
+            ShowMessage($profile['error']);
     }
-
 }
 
 
--- ulogin/sync/component.php
+++ ulogin/sync/component.php
@@ -14,69 +14,75 @@ if (!empty($_POST['token']) && $USER->isAuthorized()) {
     $s = file_get_contents('http://ulogin.ru/token.php?token=' . $_POST['token'] . '&host=' . $_SERVER['HTTP_HOST']);
     $profile = json_decode($s, true);
 
-    list($d, $m, $y) = explode('.', $profile['bdate']);
+    if (count($profile) && !isset($profile['error'])){
 
-    $arResult['USER']['LOGIN'] = Ulogin::genNickname($profile);
-    $arResult['USER']['NAME'] = $profile['first_name'];
-    $arResult['USER']['LAST_NAME'] = $profile['last_name'];
-    $arResult['USER']['EMAIL'] = $profile['email'];
-    $arResult['USER']['PERSONAL_GENDER'] = ($profile['sex'] == 2 ? 'M' : 'F');
-    $arResult['USER']['PERSONAL_CITY'] = $profile['city'];
-    $arResult['USER']['PERSONAL_BIRTHDAY'] = $d . '.' . $m . '.' . $y;
-    $arResult['USER']['EXTERNAL_AUTH_ID'] = $profile['identity'];
-    $arResult['USER']['PHOTO'] = $profile['photo'];
-    $arResult['USER']['PHOTO_BIG'] = $profile['photo_big'];
-    $arResult['USER']['NETWORK'] = $profile['network'];
-    // провер€ем есть ли пользователь в Ѕƒ.	≈сли есть - то авторизуем, нет  - регистрируем и авторизуем
-    $rsUsers = CUser::GetList(
-        ($by = "email"),
-        ($order = "desc"),
-        array(
-            "EXTERNAL_AUTH_ID" => $arResult['USER']["EXTERNAL_AUTH_ID"],
-        )
-    );
-    $arUser = $rsUsers->GetNext();
-    
+        list($d, $m, $y) = explode('.', $profile['bdate']);
 
-    if ($arUser["EXTERNAL_AUTH_ID"] == $arResult['USER']["EXTERNAL_AUTH_ID"]) {
+        $arResult['USER']['LOGIN'] = Ulogin::genNickname($profile);
+        $arResult['USER']['NAME'] = $profile['first_name'];
+        $arResult['USER']['LAST_NAME'] = $profile['last_name'];
+        $arResult['USER']['EMAIL'] = $profile['email'];
+        $arResult['USER']['PERSONAL_GENDER'] = ($profile['sex'] == 2 ? 'M' : 'F');
+        $arResult['USER']['PERSONAL_CITY'] = $profile['city'];
+        $arResult['USER']['PERSONAL_BIRTHDAY'] = $d . '.' . $m . '.' . $y;
+        $arResult['USER']['EXTERNAL_AUTH_ID'] = $profile['identity'];
+        $arResult['USER']['PHOTO'] = $profile['photo'];
+        $arResult['USER']['PHOTO_BIG'] = $profile['photo_big'];
+        $arResult['USER']['NETWORK'] = $profile['network'];
+        // провер€ем есть ли пользователь в Ѕƒ.	≈сли есть - то авторизуем, нет  - регистрируем и авторизуем
+        $rsUsers = CUser::GetList(
+            ($by = "email"),
+            ($order = "desc"),
+            array(
+                "EXTERNAL_AUTH_ID" => $arResult['USER']["EXTERNAL_AUTH_ID"],
+            )
+        );
+        $arUser = $rsUsers->GetNext();
 
-        // такой пользователь есть
 
-        $ID_INFO   = explode('=',$arUser['ADMIN_NOTES']);
-        $UloginID = $arUser['ID'];
+        if ($arUser["EXTERNAL_AUTH_ID"] == $arResult['USER']["EXTERNAL_AUTH_ID"]) {
 
-        if ($arResult['USER']['NETWORK'] == $ID_INFO[0] && $arUser['ACTIVE'] == 'Y'){//старый формат хранени€ аккаунтов, конвертируем
-            $USER->Update($arUser['ID'], array('EXTERNAL_AUTH_ID'=>''));
-            $ID_INFO[1] = $arUser['ID'];
-            $UloginID = Ulogin::createUloginAccount($arResult['USER'], $arUser['ID']);
-        }
+            // такой пользователь есть
 
-        if ($UserID != $ID_INFO[1] && !in_array($ID_INFO[0], $networks)) { //не текущий аккаунт
+            $ID_INFO   = explode('=',$arUser['ADMIN_NOTES']);
+            $UloginID = $arUser['ID'];
 
-            $networks = implode(',',$networks).','.$ID_INFO[0];
-            $second_user = CUser::GetByID($ID_INFO[1])->GetNext();
-            $second_networks = explode(',',$second_user['ADMIN_NOTES']);
-            unset($second_networks[array_search($ID_INFO[0],$second_networks)]);
-
-            if (!count($second_networks)){
-                $USER->Delete($ID_INFO[1]);
-            }else{
-                $USER->Update($ID_INFO[1],array('ADMIN_NOTES' => implode(',',$second_networks)));
+            if ($arResult['USER']['NETWORK'] == $ID_INFO[0] && $arUser['ACTIVE'] == 'Y'){//старый формат хранени€ аккаунтов, конвертируем
+                $USER->Update($arUser['ID'], array('EXTERNAL_AUTH_ID'=>''));
+                $ID_INFO[1] = $arUser['ID'];
+                $UloginID = Ulogin::createUloginAccount($arResult['USER'], $arUser['ID']);
             }
 
-            Ulogin::updateUloginAccount($UloginID, $UserID, $arResult['USER']['NETWORK']);
-            $USER->Update($UserID, array('ADMIN_NOTES'=>$networks));
+            if ($UserID != $ID_INFO[1] && !in_array($ID_INFO[0], $networks)) { //не текущий аккаунт
+
+                $networks = implode(',',$networks).','.$ID_INFO[0];
+                $second_user = CUser::GetByID($ID_INFO[1])->GetNext();
+                $second_networks = explode(',',$second_user['ADMIN_NOTES']);
+                unset($second_networks[array_search($ID_INFO[0],$second_networks)]);
+
+                if (!count($second_networks)){
+                    $USER->Delete($ID_INFO[1]);
+                }else{
+                    $USER->Update($ID_INFO[1],array('ADMIN_NOTES' => implode(',',$second_networks)));
+                }
+
+                Ulogin::updateUloginAccount($UloginID, $UserID, $arResult['USER']['NETWORK']);
+                $USER->Update($UserID, array('ADMIN_NOTES'=>$networks));
+
+            }
+
+        }else{
+
+            if (!in_array($arResult['USER']['NETWORK'], $networks)){ //добавл€ем, если такого сервиса нет
+                Ulogin::createUloginAccount($arResult['USER'], $UserID);
+                $networks = implode(',', $networks). ','.$arResult['USER']['NETWORK'];
+                $USER->Update($UserID, array('ADMIN_NOTES' => $networks));
+            }
 
         }
-
     }else{
-
-        if (!in_array($arResult['USER']['NETWORK'], $networks)){ //добавл€ем, если такого сервиса нет
-            Ulogin::createUloginAccount($arResult['USER'], $UserID);
-            $networks = implode(',', $networks). ','.$arResult['USER']['NETWORK'];
-            $USER->Update($UserID, array('ADMIN_NOTES' => $networks));
-        }
-
+        if (isset($profile['error']))
+            ShowMessage($profile['error']);
     }
 
 }
