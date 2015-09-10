6372e2d205705018344823236a081fe9e38113b8
chernozor <chernozor@yandex.ru>
2015-09-10 19:40:17 +0300

    2.0.1

--- ulogin.ulogin/lang/ru/install/index.php
+++ ulogin.ulogin/lang/ru/install/index.php
@@ -1,6 +1,6 @@
 <?
-$MESS["ulogin.ulogin_MODULE_NAME"] = "јвторизаци€ через uLogin";
-$MESS["ulogin.ulogin_MODULE_DESC"] = "";
+$MESS["ulogin.ulogin_MODULE_NAME"] = "uLogin - виджет авторизации через социальные сети";
+$MESS["ulogin.ulogin_MODULE_DESC"] = "uLogin Ч это инструмент, который позвол€ет пользовател€м получить единый доступ к различным »нтернет-сервисам без необходимости повторной регистрации, а владельцам сайтов Ч получить дополнительный приток клиентов из социальных сетей и попул€рных порталов (Google, яндекс, Mail.ru, ¬ онтакте, Facebook и др.)";
 $MESS["ulogin.ulogin_PARTNER_NAME"] = "uLogin Team";
 $MESS["ulogin.ulogin_PARTNER_URI"] = "https://ulogin.ru/";
 ?>
