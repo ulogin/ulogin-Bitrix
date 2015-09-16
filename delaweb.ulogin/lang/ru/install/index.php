f660edf84b1c3b0c5f811e3c67a71621c9032416
chernozor <chernozor@yandex.ru>
2015-09-16 12:57:46 +0300

    2.0.1

--- delaweb.ulogin/lang/ru/install/index.php
+++ delaweb.ulogin/lang/ru/install/index.php
@@ -1,6 +1,6 @@
 <?
 $MESS["delaweb.ulogin_MODULE_NAME"] = "uLogin";
-$MESS["delaweb.ulogin_MODULE_DESC"] = "виджет авторизации через социальные сети";
+$MESS["delaweb.ulogin_MODULE_DESC"] = "Виджет авторизации через социальные сети";
 $MESS["delaweb.ulogin_PARTNER_NAME"] = "DelaWeb";
 $MESS["delaweb.ulogin_PARTNER_URI"] = "http://delaweb.ru/";
 ?>
