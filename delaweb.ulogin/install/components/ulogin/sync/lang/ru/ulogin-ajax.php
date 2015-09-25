881d90d9760561dee0bcabb99490c847b9c67b58
chernozor <chernozor@yandex.ru>
2015-09-25 13:32:12 +0300

    2.0.2

--- delaweb.ulogin/install/components/ulogin/sync/lang/ru/ulogin-ajax.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS["ULOGIN_DELETE_ACCOUNT"] = "Удаление аккаунта успешно выполнено";
-$MESS["ULOGIN_ERROR_QUERY_DELETE"] = "Ошибка при выполнении запроса на удаление";
-$MESS["ULOGIN_ERROR_DELETE_ACCOUNT"] = "Ошибка при удаление аккаунта";
-?>
