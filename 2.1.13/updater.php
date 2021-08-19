04bb5b8b21c83b34e77e139eb4b3ff40192a4339
Smizuk Andrey <smizuk@mail.ru>
2021-08-19 20:25:53 +0300

    version 2.1.13

---
+++ 2.1.13/updater.php
@@ -0,0 +1,3 @@
+<?php
+$updater->CopyFiles("install/components/ulogin/auth/include/Ulogin.class.php", "components/ulogin/auth/include/Ulogin.class.php");
+$updater->CopyFiles("install/components/ulogin/sync/include/UloginSync.class.php", "components/ulogin/sync/include/UloginSync.class.php");
